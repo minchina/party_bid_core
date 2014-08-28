080d51e7f986a89f4c749d93398e2af9df009b83
minchina <minchina91@gmail.com>
2014-08-28 12:43:06 +0800

    actvity_spec passed,add :activity function

---
+++ data_structure_1/src/activity.js
@@ -0,0 +1,18 @@
+function Activity(activity_name,sign_ups,bids){
+    this.name = activity_name;
+    this.sign_ups = sign_ups || [];
+    this.bids = bids || [];
+}
+Activity.prototype.create=function(){
+    var allactivity = Activity.get_all_activity();
+    allactivity.push(this);
+    localStorage.activities = JSON.stringify(allactivity);
+};
+Activity.prototype.active=function(){
+    localStorage.current_activity = this.name;
+
+};
+Activity.get_all_activity =function(){
+  return JSON.parse(localStorage.activities) || [];
+};
+
