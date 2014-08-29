e42385b5562b754657897e3c7ba1c03fb19173e6
minchina <minchina91@gmail.com>
2014-08-29 10:31:44 +0800

    data2:Bids and Bidding Render:1 passwd

--- data_structure_2/src/render.js
+++ data_structure_2/src/render.js
@@ -2,3 +2,7 @@ function render_sign_ups(activity_name){
     var activities = Activity.get_all_activity();
     return _.find(activities,function(activity){return activity.name==activity_name}).sign_ups;
 }
+
+function transform_bids_to_view_model(activity_id){
+    return bid.get_bids_by_activity_id(activity_id);
+}
